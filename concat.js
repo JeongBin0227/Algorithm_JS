a415d6f6edafb8c0eaefdbdf7286ae3a27433a32
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-07-06 11:15:24 +0900

    [ADD] concat 활용하기

---
+++ concat.js
@@ -0,0 +1,12 @@
+//주어진 데이터를 2019/04/26 11:34:27 형식을 맞추어 출력하세요
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = year.concat('/',month,'/',day,' ',hour,':',minute,':',second)
+
+console.log(result);
+
