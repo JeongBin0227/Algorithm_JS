6e981c0d818946a8249bfdeccf195e32363383e0
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-07-10 11:12:37 +0900

    Revert "Revert "Revert "[ADD] 2-gram 추가"""
    This reverts commit 565c2b98ec8b0a19c745f787b280264aa94dd8dc.

--- countString.js
+++
@@ -1,7 +0,0 @@
-//문자열을 입력받으면 단어의 갯수를 출력하는 프로그램
-
-const countString = letter => {
-    return letter.split(' ').length
-}
-
-console.log(countString('안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다.'))
--- printReverse.js
+++
@@ -1,7 +0,0 @@
-//여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램
-
-const printReverse = num => {
-    console.log(num.split(' ').reverse().join(' '))
-}
-
-printReverse('1 2 3 4 5')
