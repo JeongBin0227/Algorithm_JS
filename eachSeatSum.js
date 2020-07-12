cab30bb9540ab9b1c0b0f21fcb48e5250f541232
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-07-12 23:43:28 +0900

    [ADD] 각자리 합 구하기

---
+++ eachSeatSum.js
@@ -0,0 +1,16 @@
+//각자리 합 구하기
+let arr = []
+let sum = 0
+
+for (let i = 0; i < 20; i++) {
+  arr[i] = i + 1
+}
+
+arr.forEach((n) => {
+  while (n !== 0) {
+    sum += n % 10
+    n = Math.floor(n / 10)
+  }
+})
+
+console.log(sum)
