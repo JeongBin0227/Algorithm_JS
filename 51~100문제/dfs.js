ac9feffe4d76fcaca7637f0ce97cd6c7b84ed28e
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-07-19 16:31:33 +0900

    [ADD] dfs 추가 공부

--- 51~100문제/dfs.js
+++ 51~100문제/dfs.js
@@ -1,30 +1,43 @@
 //다음과 같이 리스트 형태로 노드들의 연결 관계가 주어진다고 할 때 깊이 우선 탐색으로 이 노드들을 탐색했을 때의 순서를 공백으로 구분하여 출력하세요.
 
-const graph = 
-{'E': ['D', 'A'],
-'F': ['D'],
-'A': ['E', 'C', 'B'],
-'B': ['A'],
-'C': ['A'],
-'D': ['E','F']}
+// const graph = 
+// {'E': ['D', 'A'],
+// 'F': ['D'],
+// 'A': ['E', 'C', 'B'],
+// 'B': ['A'],
+// 'C': ['A'],
+// 'D': ['E','F']}
+
+const graph = {
+  'A': ['E', 'C', 'B'],
+  'B': ['A'],
+  'C': ['A'],
+  'D': ['E','F'],
+  'E': ['D', 'A'],
+  'F': ['D'],
+};
 
 function dfs(graph, start){
-    let visited = [];
-    let stack = [start];
-  
-    while (stack.length !== 0){
-      let n = stack.pop();
-      if (!visited.includes(n)){
-        visited.push(n);
-        let sub = graph[n].filter(x => !visited.includes(x));
-        for(let i of sub) {
-          stack.push(i);
-        }
+  let visited = []
+  let stack = [start]
+
+  while(stack.length !== 0){
+    let tmp = stack.pop()
+
+    if(!visited.includes(tmp)){
+      visited.push(tmp);
+      let nodes = graph[tmp].filter(node => {
+        if(!visited.includes(node)) return node
+      }
+      )
+
+      for(let node in nodes){
+        stack.push(nodes[node])
       }
     }
-    return visited;
   }
-  
+  return visited;
+}
   
   console.log(dfs(graph, 'E'));
   
